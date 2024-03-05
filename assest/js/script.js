435bbda291be597b16a3d762e4323df9d903e255
Luis <lmbibianochile@gmail.com>
2024-03-05 17:52:33 -0300

    agregamos nav bar tooltips y jquery

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,3 +1,13 @@
-$function() {
-    
-}
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+
+$(function() {
+    var titulo = $("h1");
+    titulo.on("click",function() {
+        titulo.css ("color", "red");
+    });
+});
